30f87850434e514712262c12ab81be08115d220f
Matthew Ragan <matthew.ragan@obscuradigital.com>
2017-07-20 11:29:06 -0700

    added matricies chapter

---
+++ toxFiles/shaderCode/base_matricies/glslmulti1_pixel3.glsl
@@ -0,0 +1,76 @@
+// The Book of Shaders | https://thebookofshaders.com/
+// Patricio Gonzalez Vivo | http://patriciogonzalezvivo.com/
+// Jen Lowe | http://jenlowe.net/
+
+// Ported to TouchDesigner by Matthew Ragan
+// matthewragan.com
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// As a disclaimer, I don't own any of this code. This is really
+// just a passion project to help build bridges between the larger
+// GL community and TouchDesigner developers. If you're new to GLSL
+// this is a great place to get a handle on how things work and
+// experiment.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+#define PI 3.14159265359
+
+// uniforms
+uniform float u_time;
+
+// functions
+mat2 scale(vec2 _scale){
+    return mat2(_scale.x,0.0,
+                0.0,_scale.y);
+}
+
+float box(in vec2 _st, in vec2 _size){
+    _size = vec2(0.5) - _size*0.5;
+    vec2 uv = smoothstep(_size,
+                        _size+vec2(0.001),
+                        _st);
+    uv *= smoothstep(_size,
+                    _size+vec2(0.001),
+                    vec2(1.0)-_st);
+    return uv.x*uv.y;
+}
+
+float cross(in vec2 _st, float _size){
+    return  box(_st, vec2(_size,_size/4.)) + 
+            box(_st, vec2(_size/4.,_size));
+}
+
+out vec4 fragColor;
+void main()
+{
+	// TouchDesigner provides us with a built in variable
+	// that already holds the uvs for our texutre. Normally we'd 
+	// you'll see this done other places with fragcoord and the
+	// resolution of the scene. We could similarly derive this value
+	// like this:
+	// vec2 st 		= gl_FragCoord.xy / uTD2DInfos[0].res.zw;
+	// here gl_FragCoord provides the pixel value, and uTD2DInfos[0].res.zw
+	// provides the xy resolution of our first input.
+	vec2 st 		= vUV.st;
+
+	vec3 color 		= vec3( 0.0 );
+
+    st              -= vec2(0.5);
+    st              = scale( vec2(sin(u_time)+1.0) ) * st;
+    st              += vec2(0.5);
+
+    // Show the coordinates of the space on the background
+    // color = vec3(st.x,st.y,0.0);
+
+    // Add the shape on the foreground
+    color += vec3(cross(st,0.2));
+
+
+	// TDOutputSwizzle is a TouchDesigner function that helps ensure 
+	// consistent behavior between mac and pc versions of touch. What's
+	// important to know here is that you need to provide this function
+	// with a vec4. Because our example above doesn't consider alpha, 
+	// we can construct a vec4 out of our variable color, and an additional
+	// value of 1.0 for the alpha channel.
+	fragColor 		= TDOutputSwizzle(vec4( color, 1.0 ));
+}
---
+++ toxFiles/shaderCode/base_matricies/glslmulti1_pixel4.glsl
@@ -0,0 +1,65 @@
+// The Book of Shaders | https://thebookofshaders.com/
+// Patricio Gonzalez Vivo | http://patriciogonzalezvivo.com/
+// Jen Lowe | http://jenlowe.net/
+
+// Ported to TouchDesigner by Matthew Ragan
+// matthewragan.com
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// As a disclaimer, I don't own any of this code. This is really
+// just a passion project to help build bridges between the larger
+// GL community and TouchDesigner developers. If you're new to GLSL
+// this is a great place to get a handle on how things work and
+// experiment.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+#define PI 3.14159265359
+
+// uniforms
+uniform float u_time;
+
+// functions
+// YUV to RGB matrix
+mat3 yuv2rgb = mat3(1.0, 0.0, 1.13983, 
+                    1.0, -0.39465, -0.58060, 
+                    1.0, 2.03211, 0.0);
+
+// RGB to YUV matrix
+mat3 rgb2yuv = mat3(0.2126, 0.7152, 0.0722,
+                    -0.09991, -0.33609, 0.43600, 
+                    0.615, -0.5586, -0.05639);
+
+out vec4 fragColor;
+void main()
+{
+	// TouchDesigner provides us with a built in variable
+	// that already holds the uvs for our texutre. Normally we'd 
+	// you'll see this done other places with fragcoord and the
+	// resolution of the scene. We could similarly derive this value
+	// like this:
+	// vec2 st 		= gl_FragCoord.xy / uTD2DInfos[0].res.zw;
+	// here gl_FragCoord provides the pixel value, and uTD2DInfos[0].res.zw
+	// provides the xy resolution of our first input.
+	vec2 st 		= vUV.st;
+
+	vec3 color 		= vec3( 0.0 );
+
+    // UV values goes from -1 to 1
+    // So we need to remap st (0.0 to 1.0) 
+    st              -= 0.5;  // becomes -0.5 to 0.5
+    st              *= 2.0;  // becomes -1.0 to 1.0
+
+    // we pass st as the y & z values of 
+    // a three dimentional vector to be
+    // properly multiply by a 3x3 matrix
+    color           = yuv2rgb * vec3(0.5, st.x, st.y);
+
+
+	// TDOutputSwizzle is a TouchDesigner function that helps ensure 
+	// consistent behavior between mac and pc versions of touch. What's
+	// important to know here is that you need to provide this function
+	// with a vec4. Because our example above doesn't consider alpha, 
+	// we can construct a vec4 out of our variable color, and an additional
+	// value of 1.0 for the alpha channel.
+	fragColor 		= TDOutputSwizzle(vec4( color, 1.0 ));
+}
