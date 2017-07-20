dadfc7bc1c788a8890d7e25131708bf74c7b694a
Matthew Ragan <matthew.ragan@obscuradigital.com>
2017-07-20 11:32:23 -0700

    added first bits for patterns

---
+++ toxFiles/shaderCode/base_patterns/glslmulti1_pixel1.glsl
@@ -0,0 +1,57 @@
+// The Book of Shaders | https://thebookofshaders.com/
+// Patricio Gonzalez Vivo | http://patriciogonzalezvivo.com/
+// Jen Lowe | http://jenlowe.net/
+
+// Ported to TouchDesigner by Matthew Ragan
+// matthewragan.com
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// As a disclaimer, I don't own any of this code. This is really
+// just a passion project to help build bridges between the larger
+// GL community and TouchDesigner developers. If you're new to GLSL
+// this is a great place to get a handle on how things work and
+// experiment.
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// uniforms
+uniform float u_time;
+
+// functions
+float circle(in vec2 _st, in float _radius){
+    vec2 l = _st-vec2(0.5);
+    return 1.-smoothstep(_radius-(_radius*0.01),
+                         _radius+(_radius*0.01),
+                         dot(l,l)*4.0);
+}
+
+out vec4 fragColor;
+void main()
+{
+	// TouchDesigner provides us with a built in variable
+	// that already holds the uvs for our texutre. Normally we'd 
+	// you'll see this done other places with fragcoord and the
+	// resolution of the scene. We could similarly derive this value
+	// like this:
+	// vec2 st 		= gl_FragCoord.xy / uTD2DInfos[0].res.zw;
+	// here gl_FragCoord provides the pixel value, and uTD2DInfos[0].res.zw
+	// provides the xy resolution of our first input.
+	vec2 st 		= vUV.st;
+
+	vec3 color 		= vec3( 0.0 );
+
+    st 				*= 3.0;      // Scale up the space by 3
+    st 				= fract(st); // Wrap arround 1.0
+
+    // Now we have 3 spaces that goes from 0-1
+
+    color 			= vec3(st,0.0);
+    //color 			= vec3(circle(st,0.5));
+
+	// TDOutputSwizzle is a TouchDesigner function that helps ensure 
+	// consistent behavior between mac and pc versions of touch. What's
+	// important to know here is that you need to provide this function
+	// with a vec4. Because our example above doesn't consider alpha, 
+	// we can construct a vec4 out of our variable color, and an additional
+	// value of 1.0 for the alpha channel.
+	fragColor 		= TDOutputSwizzle(vec4( color, 1.0 ));
+}
